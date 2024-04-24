5f5cc800be8b16c19bcf773e8751b0b54923a6e8
astridpot <166718625+astridpot@users.noreply.github.com>
2024-04-24 21:08:06 +0100

    added icon link form API

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector ("#wind-speed");
   let timeElement = document.querySelector ("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
 
   cityElement.innerHTML = response.data.city;
@@ -15,6 +16,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src ="${response.data.condition.icon_url}"
+  class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
@@ -24,13 +27,11 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 ];
 
 let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-
-  if (minutes < 10){
+  
+  if (minutes < 10) {
     minutes = `0${minutes}`;
 }
-
-
+return `${day} ${hours}:${minutes}`;
 }
 
 
